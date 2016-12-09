55b7faeab5acb99a6d4c6b0aecc9a9497a6fb93c
Sarah P <stps87@gmail.com>
2016-12-09 16:35:49 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+
+for (let i = 0; i < 25; i++){
+  if (i === 1){
+    console.log( array + " I am 1 strange loop.")
+  }
+  else {
+    console.log( array +  `"I am ${i} strange loops."`)
+  }
+
+}
+}
