58b076e54d4e8cb6dcc5abe7f96c8812f05400ce
Sarah P <stps87@gmail.com>
2016-12-09 18:06:40 +0000

    Done.

--- loops.js
+++ loops.js
@@ -5,8 +5,27 @@ for (let i = 0; i < 25; i++){
     console.log( array + " I am 1 strange loop.")
   }
   else {
-    console.log( array +  `"I am ${i} strange loops."`)
+    console.log( array +  `I am ${i} strange loops.`)
   }
 
 }
 }
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(--n)
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+do {
+  console.log(array.shift)}
+while (array.length > 0 && maybeTrue())
+  return array
+}
+  
